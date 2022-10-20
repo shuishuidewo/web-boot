9f35173443161edda1b4d3a7e0ee042593bf5fc9
shuishuidewo <97137312+shuishuidewo@users.noreply.github.com>
2022-10-20 17:24:01 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,40 @@
+// const { defineConfig } = require('@vue/cli-service')
+
+let proxyObj = {}
+
+proxyObj['/'] = {
+  //websocket
+  ws: false,
+  //目标地址
+  target: 'http://localhost:8081',
+  //发送请求头host会被设置target
+  changeOrigin: true,
+  //不重写请求地址
+  pathReWrite: {
+    '^/': '/'
+  }
+}
+module.exports = {
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    // proxy: {
+    //   '/': {
+    //     //websocket
+    //     ws: false,
+    //     //目标地址
+    //     target: 'http://loclhost:8081',
+    //     //发送请求头host会被设置target
+    //     changeOrigin: true,
+    //     //不重写请求地址
+    //     pathReWrite: {
+    //       '^/': '/'
+    //     }
+    //   }
+
+    // }
+    proxy: proxyObj
+  }
+}
